253104211a0683901ba07d9deb0b3e49b6ec4116
martin.ricciardi.04@gmail.com <martin.ricciardi.04@gmail.com>
2022-07-22 15:40:39 -0300

    create react mobile app

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import mainReducers from "./redux/reducers/mainReducer"
+import { Provider } from 'react-redux';
+import { configureStore as createStore } from "@reduxjs/toolkit"
 
 export default function App() {
+  const reduxStore = createStore({ reducer: mainReducers })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={reduxStore}>
+      <View style={styles.container}>
+        <Text>hooola hola hola</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
---
+++ screens/Events.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import eventsActions from "../redux/actions/eventsActions";
+import { useEffect, useState  } from 'react';
+import {useDispatch, useSelector} from 'react-redux'
+
+function Events() {
+
+    const dispatch = useDispatch()
+    const [reload, setReload] = useState(false);
+    
+    useEffect(() => {
+        dispatch(eventsActions.getEvents())
+    //     if(localStorage.getItem('token')!==null){
+    //       const token = localStorage.getItem("token")
+    //       dispatch(userActions.VerificateToken(token))
+    },[reload]
+// }
+    )
+
+    function reloadChanger() {
+        setReload(!reload);
+      }
+    
+
+    const allEvents = useSelector(store => store.eventsReducer.events)
+    console.log(allEvents)
+
+    return (
+        <Text>Nuestros eventos</Text>
+    );
+}
+export default Events;
