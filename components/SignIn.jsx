b75beef668bd8321dd91b2b2bf77776a1bdcbb8a
martin.ricciardi.04@gmail.com <martin.ricciardi.04@gmail.com>
2022-07-25 01:11:33 -0300

    panel user terminado

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -4,6 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import userActions from '../redux/actions/userActions';
+import Banner from '../assets/banner-store.jpg'
 // import bgCity from './../assets/city-body.jpg'
 // import AppLoading from 'expo-app-loading';
 // import { Comfortaa_500Medium } from '@expo-google-fonts/comfortaa';
@@ -26,7 +27,7 @@ const SignIn = () => {
         //     normal: { fontSize: 15, fontFamily: 'Comfortaa_500Medium' }
         // },
         text: {
-            primary: { color: "#00695c" },
+            primary: { color: "#f9b384d7" },
             light: { color: "#f7f3f3" },
             center: { textAlign: 'center' },
             shadowLight: {
@@ -53,7 +54,10 @@ const SignIn = () => {
         authContainer: {
             flexGrow: 1,
             justifyContent: 'center',
-            padding: 30
+            padding: 60,
+            paddingTop: 80,
+            backgroundColor: "#fae1d0",
+            height: 631
         },
         formContainer: {
             borderWidth: 1,
@@ -110,6 +114,37 @@ const SignIn = () => {
         },
     });
 
+    const { height, width } = useWindowDimensions();
+    const userStyles = StyleSheet.create({
+        storeBanner: {
+            height: height / 5,
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            padding: 30,
+            width: width
+        },
+        photo: {
+            width: 100,
+            height: 100,
+            backgroundColor: "#f9b384d7",
+            borderRadius: 100
+        },
+        container: {
+            width: "100%",
+            height: 635,
+            padding: 30,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+        },
+        user: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }
+    });
+
     async function handleSubmit() {
         const logedUser = {
             email: email,
@@ -119,53 +154,57 @@ const SignIn = () => {
         await dispatch(userActions.signInUser(logedUser));
     };
 
-    if (message !== '') {
-        setTimeout(function () {
-            navigation.navigate("Home");
-        }, 100000);
-    }
+    // if (message !== '') {
+    //     setTimeout(function () {
+    //         navigation.navigate("Home");
+    //     }, 100000);
+    // }
     // if (!fontsLoaded) {
     //     return <AppLoading />;
     // }
     return (
-        <ImageBackground style={styles.authContainer} resizeMethod='auto' resizeMode="cover" >
-            <Form
-                onButtonPress={() => handleSubmit()}
-                buttonText='SIGN IN'
-                buttonStyle={{ backgroundColor: '#00695c' }}
-                style={styles.formContainer}
-            >
-                <Text style={[styles.text.primary, { textAlign: 'center', fontSize: 30 }]}>Sign In</Text>
-                <FormItem
-                    label="Email"
-                    isRequired
-                    value={email}
-                    onChangeText={(email) => setEmail(email)}
-                    asterik
-                    showErrorIcon
-                    textContentType='emailAddress'
-                    keyboardType='email-address'
-                    textInputStyle={{ color: '#00695c' }}
-                />
-                <FormItem
-                    label="Password"
-                    isRequired
-                    value={password}
-                    onChangeText={(pass) => setPassword(pass)}
-                    asterik
-                    showErrorIcon
-                    textContentType='password'
-                    secureTextEntry
-                />
-                <View>
-                    <Text style={{ textAlign: 'center', fontSize: 15, marginTop: 30 }}>Don't have an account?</Text>
-                    <TouchableOpacity underlayColor="#000" activeOpacity={0.6} onPress={() => navigation.navigate("SignUp")}>
-                        <Text style={[styles.text.primary, { textAlign: 'center', fontSize: 30, textDecorationLine:'underline' }]}>Sign Up!</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </Form>
-        </ImageBackground>
+        <>
+            <ImageBackground style={userStyles.storeBanner} source={Banner} resizeMethod='auto' resizeMode="cover" >
+                        <Text variant='h2' style={{ fontSize: 20, backgroundColor: '#f9b384d7', padding: 10, borderRadius: 10 }} >Mi Perfil</Text>
+                </ImageBackground>
+            <ImageBackground style={styles.authContainer} resizeMethod='auto' resizeMode="cover" >
+                <Form
+                    onButtonPress={() => handleSubmit()}
+                    buttonText='Ingresar'
+                    buttonStyle={{ backgroundColor: '#f9b384d7' }}
+                    style={styles.formContainer}
+                >
+                    <Text style={[styles.text.primary, { textAlign: 'center', fontSize: 30 }]}>Inicia sesión</Text>
+                    <FormItem
+                        label="Email"
+                        isRequired
+                        value={email}
+                        onChangeText={(email) => setEmail(email)}
+                        asterik
+                        showErrorIcon
+                        textContentType='emailAddress'
+                        keyboardType='email-address'
+                        textInputStyle={{ color: '#f9b384d7' }}
+                    />
+                    <FormItem
+                        label="Contraseña"
+                        isRequired
+                        value={password}
+                        onChangeText={(pass) => setPassword(pass)}
+                        asterik
+                        showErrorIcon
+                        textContentType='password'
+                        secureTextEntry
+                    />
+                    <View>
+                        <Text style={{ textAlign: 'center', fontSize: 15, marginTop: 30 }}>No tenes una cuenta?</Text>
+                        <TouchableOpacity underlayColor="#000" activeOpacity={0.6} onPress={() => navigation.navigate("SignUp")}>
+                            <Text style={[styles.text.primary, { textAlign: 'center', fontSize: 30, textDecorationLine:'underline' }]}>Registrate!</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Form>
+            </ImageBackground>
+        </>
     )
 }
 export default SignIn;
--- components/User.jsx
+++ components/User.jsx
@@ -1,15 +1,66 @@
 import React from "react";
-import { StyleSheet, useWindowDimensions, ImageBackground, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, useWindowDimensions, ImageBackground, View, Text, TouchableOpacity, ScrollView, Button } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
-import Hero from "./Hero";
+import Banner from '../assets/banner-store.jpg'
+import { useSelector } from "react-redux";
+import { AsyncStorage } from 'react-native';
 
 const User = () =>{
+    const userImage = useSelector(store => store.userReducer.user.image)
+    const userFName = useSelector(store => store.userReducer.user.firstName)
+    const userLName = useSelector(store => store.userReducer.user.lastName)
+    const navigation = useNavigation();
+
+    const { height, width } = useWindowDimensions();
+    const userStyles = StyleSheet.create({
+        storeBanner: {
+            height: height / 5,
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            padding: 30,
+            width: width
+        },
+        photo: {
+            width: 100,
+            height: 100,
+            backgroundColor: "#f9b384d7",
+            borderRadius: 100
+        },
+        container: {
+            width: "100%",
+            height: 635,
+            padding: 30,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+        },
+        user: {
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }
+    });
+
     return(
         <>
-        <Hero/>
-        <Text>
-            pibe estas logeado 👍
-        </Text>
+            <ImageBackground style={userStyles.storeBanner} source={Banner} resizeMethod='auto' resizeMode="cover" >
+                    <Text variant='h2' style={{ fontSize: 20, backgroundColor: '#f9b384d7', padding: 10, borderRadius: 10 }} >Mi Perfil</Text>
+            </ImageBackground>
+                <View style={userStyles.container}>
+                    <View style={userStyles.user}>
+                        <ImageBackground style={userStyles.photo} source={userImage}/>
+                        <Text style={{ fontSize: 40 }}>
+                            {userFName + " " + userLName}
+                        </Text>
+                    </View>
+                    <View style={{ justifyContent: "center", alignSelf: "center" }}>
+                        <TouchableOpacity onPress={ () => {AsyncStorage.removeItem('@token') && navigation.navigate("Home")}} underlayColor="#000" activeOpacity={0.6} style={{ backgroundColor: "#f9b384d7", height: 50, width: 130, justifyContent: "center", alignItems: "center", borderRadius: 15}}>
+                            <Text style={{fontSize: 20}}>Cerrar sesión</Text>
+                        </TouchableOpacity>
+                    </View>
+                    
+                </View>
         </>
     )
 }
